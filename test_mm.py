f29913e7154ba514eab2ba27c5e199d9b2c1fe4c
valacuhi <valacuhi@gmail.com>
2026-03-12 21:11:49 -0700

    Move obsolete files to junk_py and update env template

--- test_mm.py
+++
@@ -1,8 +0,0 @@
-from game_logic import GameState
-from agents.minimax_agent import MinimaxAgent
-
-gs = GameState()
-gs.make_move(7, 1, 1)  # Cat plays in middle
-mm = MinimaxAgent(depth=4)
-move = mm.get_move(gs, 2)
-print("Minimax Move:", move)
